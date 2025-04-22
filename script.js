d31a1873a10b597f0fb2de1998dea92ef4bdc537
MusicSociety <albygiuly72@gmail.com>
2025-04-22 15:43:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,21 +4,30 @@ const audioPlayer = document.getElementById('audioPlayer');
 
 let videoStarted = false;
 
+// iOS workaround per sbloccare il contesto audio
+const unlockAudio = () => {
+  const silentAudio = new Audio();
+  silentAudio.play().catch(() => {}); // forza il contesto audio anche se fallisce
+  document.removeEventListener('click', unlockAudio);
+};
+document.addEventListener('click', unlockAudio);
+
 playButton.addEventListener('click', () => {
   if (audioPlayer.paused) {
     if (!videoStarted) {
       video.muted = false;
-      video.play();
+      video.play().catch(err => console.log("Video play error:", err));
       videoStarted = true;
     } else {
-      video.play(); // se era stato messo in pausa, riprendilo
+      video.play().catch(err => console.log("Video resume error:", err));
     }
 
-    audioPlayer.play();
+    audioPlayer.play().catch(err => console.log("Audio play error:", err));
     playButton.classList.add('glow');
   } else {
     audioPlayer.pause();
-    video.pause(); // ferma anche il video
+    video.pause();
     playButton.classList.remove('glow');
   }
 });
+
