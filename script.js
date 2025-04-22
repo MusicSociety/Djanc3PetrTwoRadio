ec4b79ecafb47f3c06cea4b87e6df1078e7830bc
MusicSociety <albygiuly72@gmail.com>
2025-04-22 15:37:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,23 @@ const playButton = document.getElementById('playButton');
 const video = document.getElementById('mainVideo');
 const audioPlayer = document.getElementById('audioPlayer');
 
+let videoStarted = false;
+
 playButton.addEventListener('click', () => {
   if (audioPlayer.paused) {
-    video.muted = false;
-    video.play();
+    if (!videoStarted) {
+      video.muted = false;
+      video.play();
+      videoStarted = true;
+    } else {
+      video.play(); // se era stato messo in pausa, riprendilo
+    }
+
     audioPlayer.play();
     playButton.classList.add('glow');
   } else {
-    video.pause();
     audioPlayer.pause();
+    video.pause(); // ferma anche il video
     playButton.classList.remove('glow');
   }
 });
